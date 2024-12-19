89625f17fe1b00d1a76fe96d8045ffccbc96514e
Riciere Marcone <85197053+ricichien@users.noreply.github.com>
2024-12-19 14:21:58 -0300

    commiting files

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Container(
+    child: Center(
+        child: Text('Flutterando',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 50.0))),
+  ));
+}
+
+class AppWidget {}
